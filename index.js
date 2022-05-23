e57d5777163a6bf4f63f834c8a25a008868e14bc
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-05-23 17:46:14 -0300

    adicionando arquivo transpilado

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+const primeiroNome = 'seu nome';
+const diaDoMes = 12;
+const hojeFezSol = true;
+const cachorro = [
+    { nome: 'Kin',
+        idade: 13,
+        raca: 'Schnauzer',
+        grandePorte: false,
+        donos: ['Maria Luiza', 'Guilherme'] }
+];
+const animal = [
+    { tipo: 'gato',
+        bipede: false,
+        asas: false,
+        regiao: 'sjdjdf'
+    }
+];
+const humano = () => {
+    return {
+        nome: 'João',
+        idade: 30
+    };
+};
+const outroHumano = () => {
+    return {
+        nome: 'José'
+    };
+};
+const maisUmHumano = {
+    nome: 'Maria',
+    altura: 1.67
+};
