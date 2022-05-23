ba745d6d6857a96953933e4f16476cdfa650b796
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-05-23 17:42:53 -0300

    exercicio typescript

--- index.ts
+++ index.ts
@@ -16,3 +16,45 @@ interface Animais {
     asas: boolean,
     regiao: string
 }
+
+const cachorro: Array<Cachorro> = [
+    {nome: 'Kin',
+     idade: 13,
+     raca:'Schnauzer', 
+     grandePorte: false, 
+     donos: ['Maria Luiza', 'Guilherme']}
+];
+
+const animal: Array<Animais> =[
+    {tipo: 'gato',
+     bipede: false,
+     asas: false,
+     regiao: 'sjdjdf'  
+    }
+]
+
+type Humano = {
+    nome: string,
+    idade: number,
+    altura: number,
+}
+
+const humano = (): Omit<Humano, 'altura'> => {
+    return {
+        nome: 'João',
+        idade: 30
+    }
+}
+
+const outroHumano = (): Partial<Humano> => {
+    return {
+       nome: 'José'
+    }
+}
+
+type Humano2 = Omit<Humano, 'idade'>;
+
+const maisUmHumano: Humano2 = {
+    nome: 'Maria',
+    altura: 1.67
+}
